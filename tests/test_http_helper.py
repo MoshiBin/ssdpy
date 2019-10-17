d3450746fbf3b2942b4db01552aad3e9ca89aac1
Moshi Binyamini <MoshiBin@users.noreply.github.com>
2019-10-17 20:32:25 +0300

    Add notify payload generator

--- tests/test_http_helper.py
+++ tests/test_http_helper.py
@@ -9,14 +9,20 @@ class TestHTTPHelper(unittest.TestCase):
         good_response = b"HTTP/1.1 200 OK\r\n" b"MX: 5\r\n"
         headers = parse_headers(good_response)
         self.assertEqual("5", headers.get("mx"))
+
+    def test_parse_headers_invalid_header(self):
+        good_response = b"HTTP/1.1 200 OK\r\n" b"MX: 5\r\n"
+        headers = parse_headers(good_response)
         self.assertEqual(None, headers.get("should-not-exist"))
 
+    def test_parse_headers_invalid_response(self):
         bad_response = b"not an http response"
         with self.assertRaises(ValueError):
             parse_headers(bad_response)
 
-        another_bad_response = (
+    def test_parse_headers_bad_response_header(self):
+        bad_response = (
             b"HTTP/1.1 200 OK\r\n" b"Header: OK\r\n" b"Another-header-not-ok\r\n"
         )
         with self.assertRaises(ValueError):
-            parse_headers(another_bad_response)
+            parse_headers(bad_response)
--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 import unittest
-from ssdpy.protocol import create_msearch_payload
+from ssdpy.protocol import create_msearch_payload, create_notify_payload
 from ssdpy.http_helper import parse_headers
 
 
@@ -13,3 +13,51 @@ class TestProtocol(unittest.TestCase):
         self.assertEqual(data_headers.get("st"), "ssdp:all")
         self.assertEqual(data_headers.get("man"), "ssdp:discover")
         self.assertEqual(data_headers.get("mx"), "1")
+
+    def test_notify_payload(self):
+        data = create_notify_payload("239.255.255.250:1900", "testdevice", "ssdpy-test")
+        data_headers = parse_headers(data)
+        self.assertEqual(data_headers.get("host"), "239.255.255.250:1900")
+        self.assertEqual(data_headers.get("nt"), "testdevice")
+        self.assertEqual(data_headers.get("usn"), "ssdpy-test")
+        self.assertIsNone(data_headers.get("non-existant-header"))
+
+    def test_notify_location(self):
+        data = create_notify_payload(
+            "239.255.255.250:1900",
+            "testdevice",
+            "ssdpy-test",
+            location="http://localhost",
+        )
+        data_headers = parse_headers(data)
+        self.assertEqual(data_headers.get("host"), "239.255.255.250:1900")
+        self.assertEqual(data_headers.get("nt"), "testdevice")
+        self.assertEqual(data_headers.get("usn"), "ssdpy-test")
+        self.assertIsNone(data_headers.get("non-existant-header"))
+        self.assertEqual(data_headers.get("location"), "http://localhost")
+
+    def test_notify_al(self):
+        data = create_notify_payload(
+            "239.255.255.250:1900", "testdevice", "ssdpy-test", al="http://localhost"
+        )
+        data_headers = parse_headers(data)
+        self.assertEqual(data_headers.get("host"), "239.255.255.250:1900")
+        self.assertEqual(data_headers.get("nt"), "testdevice")
+        self.assertEqual(data_headers.get("usn"), "ssdpy-test")
+        self.assertIsNone(data_headers.get("non-existant-header"))
+        self.assertEqual(data_headers.get("al"), "http://localhost")
+
+    def test_notify_age(self):
+        data = create_notify_payload(
+            "239.255.255.250:1900", "testdevice", "ssdpy-test", max_age=999
+        )
+        data_headers = parse_headers(data)
+        self.assertEqual(data_headers.get("host"), "239.255.255.250:1900")
+        self.assertEqual(data_headers.get("nt"), "testdevice")
+        self.assertEqual(data_headers.get("usn"), "ssdpy-test")
+        self.assertIsNone(data_headers.get("non-existant-header"))
+        self.assertEqual(data_headers.get("cache-control"), "max-age=999")
+
+    def test_notify_edge_cases(self):
+        with self.assertRaises(ValueError):
+            create_notify_payload("x", "y", "z", max_age="not-a-number")
