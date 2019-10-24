e631a4049275a10f6af0a64ece95baaec9f58cf3
Moshi Binyamini <MoshiBin@users.noreply.github.com>
2019-10-24 18:36:26 +0300

    Add SSDPClient and SSDPServer to __all__

--- ssdpy/__init__.py
+++ ssdpy/__init__.py
@@ -2,5 +2,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .version import VERSION
+from .server import SSDPServer
+from .client import SSDPClient
 
 __version__ = VERSION
+
+__all__ = [SSDPServer, SSDPClient]
