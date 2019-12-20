70ffbf155bff21d371b10086674721920c4990e3
Moshi Binyamini <MoshiBin@users.noreply.github.com>
2019-12-20 14:53:16 +0200

    Migrate test_server to pytest #36

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,28 +2,30 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import threading
 import time
-import unittest
+import pytest
+import os
 from ssdpy import SSDPServer
 
 
-class TestProtocol(unittest.TestCase):
-    def test_server_ipv4(self):
-        server = SSDPServer("test-server", proto="ipv4")
-        server.sock.settimeout(5)
-        server_thread = threading.Thread(target=server.serve_forever)
-        server_thread.start()
-        time.sleep(0.5)
-        server.stopped = True
-        server_thread.join()
+def test_server_ipv4():
+    server = SSDPServer("test-server", proto="ipv4")
+    server.sock.settimeout(5)
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.start()
+    time.sleep(0.5)
+    server.stopped = True
+    server_thread.join()
 
-    # Skip this test - IPv6 testing is broken for Travis-CI
-    # see https://github.com/travis-ci/travis-ci/issues/8361 for more info
-    @unittest.SkipTest
-    def test_server_ipv6(self):
-        server = SSDPServer("test-server-ipv6", proto="ipv6")
-        server.sock.settimeout(5)
-        server_thread = threading.Thread(target=server.serve_forever)
-        server_thread.start()
-        time.sleep(0.5)
-        server.stopped = True
-        server_thread.join()
+
+@pytest.mark.skipif(
+    os.environ.get("TRAVIS") == "true",
+    reason="IPv6 testing is broken in Travis-CI, see https://github.com/travis-ci/travis-ci/issues/8361",
+)
+def test_server_ipv6():
+    server = SSDPServer("test-server-ipv6", proto="ipv6")
+    server.sock.settimeout(5)
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.start()
+    time.sleep(0.5)
+    server.stopped = True
+    server_thread.join()
