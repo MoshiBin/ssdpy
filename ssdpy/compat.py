47869e91a72c9f1371ff78dd4d39a401a0ffb9fc
Moshi Binyamini <MoshiBin@users.noreply.github.com>
2020-12-22 22:13:02 +0200

    Avoid if_nametoindex on Windows

--- ssdpy/compat.py
+++ ssdpy/compat.py
@@ -8,32 +8,37 @@ import sys
 
 PY2 = sys.version_info[0] == 2
 WINDOWS = sys.platform == "win32"
+LINUX = sys.platform == "linux"
 
 string_types = basestring if PY2 else str  # noqa
 
 
 # Python 2 doesn't have socket.if_nametoindex so we need to implement it manually
-if PY2:
-    import ctypes.util
+if LINUX:
+    if PY2:
+        import ctypes.util
 
-    libc = ctypes.CDLL(ctypes.util.find_library("c"))
+        libc = ctypes.CDLL(ctypes.util.find_library("c"))
+
+        def if_nametoindex(name):
+            """
+            Return the logical index number of the given interface name.
+            This is backported for Python 2.7 on Linux.
+            """
+            if not isinstance(name, string_types):
+                raise TypeError("Expected string type, got '{}'".format(type(name)))
+
+            rc = libc.if_nametoindex(name)
+            if rc == 0:
+                raise OSError("no interface with this name '{}'".format(name))
+            return rc
+
+    else:
+        if_nametoindex = socket.if_nametoindex
+else:
 
     def if_nametoindex(name):
-        """
-        Return the logical index number of the given interface name.
-        This is backported for Python 2.7 on Linux.
-        """
-        if not isinstance(name, string_types):
-            raise TypeError("Expected string type, got '{}'".format(type(name)))
-
-        rc = libc.if_nametoindex(name)
-        if rc == 0:
-            raise OSError("no interface with this name '{}'".format(name))
-        return rc
-
-
-else:
-    if_nametoindex = socket.if_nametoindex
+        raise NotImplementedError("if_nametoindex is not available on this platform")
 
 
 SO_BINDTODEVICE = getattr(socket, "SO_BINDTODEVICE", 25)
