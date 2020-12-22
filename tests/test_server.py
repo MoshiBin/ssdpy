6d4dcce0f26019bef63d25858f1e0e25f8f8f2d1
Moshi Binyamini <MoshiBin@users.noreply.github.com>
2020-12-22 23:30:23 +0200

    Increase coverage for server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -7,6 +7,7 @@ import errno
 import os
 import sys
 from ssdpy import SSDPServer
+from ssdpy.compat import LINUX
 
 
 def test_server_ipv4():
@@ -79,3 +80,9 @@ def test_server_extra_fields():
 def test_server_extra_fields_non_ascii():
     with pytest.raises(ValueError):
         SSDPServer("test-server", extra_fields={"invalid-field™": "foo"})
+
+
+@pytest.mark.skipif(LINUX, reason="This limitation only occurs on Windows")
+def test_server_bind_interface_in_windows():
+    with pytest.raises(ValueError):
+        SSDPServer("test-server", iface=b"Ethernet")
