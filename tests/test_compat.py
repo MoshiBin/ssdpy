421c50ca09041883fce335062dc3ba8e6fed30a0
Moshi Binyamini <MoshiBin@users.noreply.github.com>
2020-12-22 23:23:34 +0200

    Add tests for inet_pton

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -2,8 +2,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import pytest
 import sys
+import socket
 from .compat import PY2
-from ssdpy.compat import if_nametoindex
+from ssdpy.compat import if_nametoindex, inet_pton, WINDOWS
 
 
 @pytest.mark.skipif(sys.platform == "win32", reason="No bind to interface on Windows")
@@ -29,3 +30,22 @@ def test_if_nametoindex_nodevice():
 @pytest.mark.skipif(sys.platform == "win32", reason="No bind to interface on Windows")
 def test_if_nametoindex():
     assert type(if_nametoindex(b"lo")) is int
+
+
+@pytest.mark.skipif(not (PY2 and WINDOWS), reason="Only test compat inet_pton")
+def test_inet_pton():
+    assert (
+        inet_pton(socket.AF_INET6, "dead::beef") == b"\xde\xad\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xbe\xef"
+    )
+
+
+@pytest.mark.skipif(not (PY2 and WINDOWS), reason="Only test compat inet_pton")
+def test_inet_pton_bad_addr():
+    with pytest.raises(OSError):
+        inet_pton(socket.AF_INET6, "bad address")
+
+
+@pytest.mark.skipif(not (PY2 and WINDOWS), reason="Only test compat inet_pton")
+def test_inet_pton_wrong_proto():
+    with pytest.raises(ValueError):
+        inet_pton(socket.AF_INET, "127.0.0.1")
