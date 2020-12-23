138ccbb033e0d6bf36c6d94214c7a0e313be5a0b
Moshi Binyamini <MoshiBin@users.noreply.github.com>
2020-12-23 21:49:20 +0200

    ssdpy-discover: Set the default ST to ssdp:all
    Fixes ssdpy-discover: error: too few arguments #51

--- ssdpy/cli/client.py
+++ ssdpy/cli/client.py
@@ -12,40 +12,18 @@ logging.basicConfig()
 
 
 def parse_args(argv):
-    parser = argparse.ArgumentParser(description="Run an SSDP M-SEARCH",)
-    parser.add_argument(
-        "-V", "--version", action="version", version="%(prog)s {}".format(VERSION)
-    )
+    parser = argparse.ArgumentParser(description="Run an SSDP M-SEARCH")
+    parser.add_argument("-V", "--version", action="version", version="%(prog)s {}".format(VERSION))
     parser.add_argument("-v", "--verbose", help="Be more verbose", action="store_true")
-    parser.add_argument(
-        "-6", "--ipv6", help="Listen on IPv6 instead of IPv4", action="store_true"
-    )
-    parser.add_argument(
-        "-t", "--ttl", help="TTL for the M-SEARCH (default: 2)", default=2, type=int
-    )
-    parser.add_argument(
-        "-o",
-        "--timeout",
-        help="Maximum timeout for connections (default: 5)",
-        default=5,
-        type=int,
-    )
-    parser.add_argument("ST", help="Type of device to search for (ST)", nargs=1)
+    parser.add_argument("-6", "--ipv6", help="Listen on IPv6 instead of IPv4", action="store_true")
+    parser.add_argument("-t", "--ttl", help="TTL for the M-SEARCH (default: 2)", default=2, type=int)
+    parser.add_argument("-o", "--timeout", help="Maximum timeout for connections (default: 5)", default=5, type=int)
+    parser.add_argument("ST", help="Type of device to search for (ST)", nargs=1, default="ssdp:all")
     parser.add_argument("-i", "--iface", help="Listen on a specific network interface")
     parser.add_argument("-a", "--address", help="Bind to this address")
-    parser.add_argument(
-        "-p", "--port", help="Send on this port (default: 1900)", default=1900, type=int
-    )
-    parser.add_argument(
-        "-m",
-        "--mx",
-        help="Maximum wait time for response (default 1s)",
-        default=1,
-        type=int,
-    )
-    parser.add_argument(
-        "-j", "--json", help="Format output as JSON", action="store_true",
-    )
+    parser.add_argument("-p", "--port", help="Send on this port (default: 1900)", default=1900, type=int)
+    parser.add_argument("-m", "--mx", help="Maximum wait time for response (default 1s)", default=1, type=int)
+    parser.add_argument("-j", "--json", help="Format output as JSON", action="store_true")
     return parser.parse_args(argv)
 
 
