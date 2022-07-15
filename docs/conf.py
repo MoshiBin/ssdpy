5c731d9e1a73bc8a7009b42941b0787c92ad836e
Moshi Binyamini <MoshiBin@users.noreply.github.com>
2022-07-15 23:37:57 +0300

    📄 Update license year

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "SSDPy"
-copyright = "2020, Moshi Binyamini"
+copyright = "2022, Moshi Binyamini"
 author = "Moshi Binyamini"
 
 # The full version, including alpha/beta/rc tags
